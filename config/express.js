b1c56dbf1c9685b44ef6656150985a92053ca91c
Ahmed Ali <ahmed.ali@softvira.com>
2025-02-18 18:26:23 +0500

    increase in size

--- config/express.js
+++ config/express.js
@@ -3,7 +3,8 @@ var cors = require("cors");
 var app = express();
 app.use(express.static("public"));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Increase payload size limit
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 module.exports = app;
